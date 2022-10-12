127e976c755ef50f24a150af47898707e7615701
Molodoi Cheloveck <vasilisk.factori1999@gmail.com>
2022-10-12 12:26:16 +0500

    On branch main  Your branch and 'origin/main' have diverged,  and have 21 and 1 different commits each, respectively.  Changes to be committed: 	new file:   ip.py 	new file:   test.py

---
+++ ip.py
@@ -0,0 +1,16 @@
+import readline
+
+class stuff():
+    def ip(self):
+        a=''
+        return a
+    def user(self):    
+        login=b''  
+        password=b''
+        return(login,password)
+    def description(self):    
+        decription=readline
+        return (decription)
+    def vlan_id(self):
+        vlan=readline
+        return(vlan)
